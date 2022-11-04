26cdb862999b64bf0960f4c7183688d929d3f1ae
raikamna <kamnarai9725@gmail.com>
2022-11-04 14:39:21 +0530

    second commit

---
+++ src/main/java/Baseclass.java
@@ -0,0 +1,29 @@
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class Baseclass {
+	
+	public static WebDriver driver;
+	public String baseURL = "https://www.google.com/";
+	@BeforeClass
+	public void setUp() 
+	{
+		System.setProperty("webdriver.chrome.driver", "E:\\Automation\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+	}
+	
+
+	@AfterClass
+	public void down() throws Exception
+	{		
+		Thread.sleep(50);
+		driver.quit();
+	}
+
+}
